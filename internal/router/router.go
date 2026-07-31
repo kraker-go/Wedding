8f3f85aa3e8f159df81c19e19811499aa03e98fb
mr.rock8438_@gmal.com <mr.rock8438@gmail.com>
2026-07-31 13:31:47 +0300

    каркас для сайта

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,11 +2,22 @@ package router
 
 import (
 	"github.com/gorilla/mux"
+	"net/http"
 	"wedding/internal/handler"
 )
 
 func InitRouter(hand *handler.UserHandler) (*mux.Router, error) {
 	router := mux.NewRouter()
+
+	// ✅ Отдаём HTML-страницу
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./HTML/index.html")
+	}).Methods("GET")
+
+	// ✅ Отдаём CSS, JS, картинки (если есть)
+	router.PathPrefix("/static/").Handler(
+		http.StripPrefix("/static/", http.FileServer(http.Dir("./HTML/static/"))),
+	)
 
 	router.HandleFunc("/user", hand.AddUserHandler).Methods("POST")
 
