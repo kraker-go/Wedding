f051a4ade938648b4481be6bd801918fd3f7f4f1
mr.rock8438_@gmal.com <mr.rock8438@gmail.com>
2026-07-31 19:56:25 +0300

    фикс для мобильных устройств

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,28 +4,8 @@ import (
 	"os"
 )
 
-type Postgres struct {
-	PORT     string
-	HOST     string
-	USER     string
-	PASSWORD string
-	DBNAME   string
-	SSL_MODE string
-}
-
 type Server struct {
 	Port string
-}
-
-func CfgInit() (*Postgres, error) {
-	return &Postgres{
-		PORT:     os.Getenv("DB_PORT"),
-		HOST:     os.Getenv("DB_HOST"),
-		USER:     os.Getenv("DB_USER"),
-		PASSWORD: os.Getenv("DB_PASSWORD"),
-		DBNAME:   os.Getenv("DB_DBNAME"),
-		SSL_MODE: os.Getenv("DB_SSL_MODE"),
-	}, nil
 }
 
 func ServerInit() (*Server, error) {
